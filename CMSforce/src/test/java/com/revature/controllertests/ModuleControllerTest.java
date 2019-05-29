377ba5ad93a44cd167d9c257fee3d4043fd0dd03
J D <john_david_orr@yahoo.com>
2019-05-28 21:00:13 -0400

    issue with created time. Need more testing

--- CMSforce/src/test/java/com/revature/controllertests/ModuleControllerTest.java
+++ CMSforce/src/test/java/com/revature/controllertests/ModuleControllerTest.java
@@ -1,5 +1,6 @@
 package com.revature.controllertests;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -43,20 +44,55 @@ public class ModuleControllerTest {
 
 	@Autowired
 	Gson gson;
-
+//	public Set<Module> getAllModules();
+//	
+//	public Module getModuleById(int id);
+//	
+	
+//	@Test
+//	void createModule() throws Exception {
+//		String JSON = "{\"id\": 1,\"subject\": \"Fakey McFake\",\"created\": 0,\"links\": null}";
+//		Module module = gson.fromJson(JSON, Module.class);
+//		Mockito.when(ms.createModule(module)).thenReturn(module);
+//		ResultActions resultActions = mockmvc
+//				.perform(post("/module").contentType(MediaType.APPLICATION_JSON_VALUE).content(JSON));
+//		resultActions.andExpect(status().isOk());
+//		String JOH = gson.toJson(module);
+//		resultActions.andExpect(content().string(JOH));
+//	}
 	@Test
-	void createContent() throws Exception {
-		String JSON = "{\"id\":88,\"title\":\"Dadaism\",\"format\":\"Ironing Board\",\"description\":\"Anti-art\",\"url\":\"www.dadaism.test\",\"links\":[{\"id\":12,\"contentId\":88,\"moduleId\":3,\"affiliation\":\"relaventTo\"}]}";
-		Content cont = gson.fromJson(JSON, Content.class);
-		Mockito.when(ms.createModule(module).createContent(cont)).thenReturn(cont);
+	void doubleCreationModule() throws Exception {
+		String JSON = "{\"id\":1,\"subject\":\"Fakey McFake\",\"created\":0,\"links\": null}";
+		
+//		ResultActions resultActions2 = mockmvc
+//				.perform(post("/module").contentType(MediaType.APPLICATION_JSON_VALUE).content(JSON));
+		
+		
+		Module module = gson.fromJson(JSON, Module.class);
+		Module realmodule = fakeModules().iterator().next();
+		Mockito.when(ms.createModule(realmodule)).thenReturn(realmodule);
 		ResultActions resultActions = mockmvc
-				.perform(post("/content").contentType(MediaType.APPLICATION_JSON_VALUE).content(JSON));
+				.perform(post("/module").contentType(MediaType.APPLICATION_JSON_VALUE).content(JSON));
+		String JOH = gson.toJson(module);
 		resultActions.andExpect(status().isOk());
-		String JOH = gson.toJson(cont);
 		resultActions.andExpect(content().string(JOH));
 	}
+	@Test
+	void getAllModules() throws Exception {
 
-	private Set<Module> fakeContents() {
+		Mockito.when(ms.getAllModules()).thenReturn(this.fakeModules());
+		long time = System.currentTimeMillis();
+		ResultActions resultActions = mockmvc.perform(get("/module"));	
+		long time2 = System.currentTimeMillis();
+		long diff = (time2 - time);
+		time = time - diff;
+		resultActions.andExpect(status().isOk());
+//		resultActions.andExpect(content().json("[{\"id\":1,\"subject\":\"Fakey McFake\",\"created\":" + time + ",\"links\": null},{\"id\": 2,\"subject\": \"Mocky McMock\",\"created\":"+ time +",\"links\": null},{\"id\": 3,\"subject\": \"Pastiche McPastiche\",\"created\":"+ time +",\"links\": null}]"));
+//		System.err.println(resultActions.andReturn().getResponse().getContentAsString());
+		
+	} 
+
+	private Set<Module> fakeModules() {
 		Module mod1 = new Module(1, "Fakey McFake", 0, null);
 		Module mod2 = new Module(2, "Mocky McMock", 0, null);
 		Module mod3 = new Module(3, "Pastiche McPastiche", 0, null);
@@ -65,6 +101,7 @@ public class ModuleControllerTest {
 		modules.add(mod1);
 		modules.add(mod2);
 		modules.add(mod3);
+		System.out.println(modules);
 
 		return modules;
 
